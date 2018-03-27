7ea5c22b46126f01fedd8ea353c1865dce7771a5
Alex Hinds <alex.hinds141@gmail.com>
2018-03-27 14:19:46 +1100

    week5 labs

---
+++ static/comp1521/tuesday/week5/space.s
@@ -0,0 +1,22 @@
+int v0;
+int v1 = 42;
+char v2;
+char v3 = 'a';
+double v4;
+int v5[20];
+int v6[10][5];
+struct { int x; int y; } v7;
+struct { int x; int y; } v8[4];
+struct { int x; int y; } *v9[4];
+
+.data
+v0: .space 4
+v1: .word 42
+v2: .space 1
+v3: .byte 'a'
+v4: .space 8
+v5: .space 80
+v6: .space 200
+v7: .space 8
+v8: .space 32
+v9: .space 16
