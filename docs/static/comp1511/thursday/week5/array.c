ab99eedb6b429fb5d3c00e945dd2acc303a043f6
Alex Hinds <alex.hinds141@gmail.com>
2018-03-28 11:02:41 +1100

    with week5 material

--- docs/static/comp1511/thursday/week5/array.c
+++ docs/static/comp1511/thursday/week5/array.c
@@ -1,10 +1,18 @@
 #include "array.h"
 #include <stdio.h>
 
-void printArray(int array[], int len) {
-   for (unsigned int i = 0; i < len; i++) {
+void initArray(int array[], int length) {
+   int i = 0;
+   while (i < length) {
+      array[i] = i+1;
+      i++;
+   }
+}
+
+void printArray(int array[], int length) {
+   for (int i = 0; i < length; i++) {
       printf("%d", array[i]);
-      if (i < len) {
+      if (i < length) {
          printf(" ");
       }
    }
--- docs/static/comp1511/thursday/week5/map.c
+++ docs/static/comp1511/thursday/week5/map.c
@@ -1,1 +1,15 @@
 #include "array.h"
+#include <stdio.h>
+
+/* multiply everything by 2 in array */
+void multArray(int array[], int length) {
+   int i = 0;
+
+   while (i < length) {
+      // array[i] = array[i] *2;
+      int current = array[i];
+      current = current * 2;
+      array[i] = current;
+      i++;
+   }
+}
