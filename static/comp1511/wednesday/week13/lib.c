cdcdb8714fb825ba67c2e1cbcb6ab5bc023cc174
Alex Hinds <alex.hinds141@gmail.com>
2018-05-29 16:36:54 +1000

    updates to include latest labssss

---
+++ static/comp1511/wednesday/week13/lib.c
@@ -0,0 +1,108 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void run_tests(void) {
+
+   printf("Beginning Tests\n");
+   Node head = new_node(0);
+   for (int i = 1; i < 5; i++) {
+      assert(length(head) == i);
+      head = list_append(head, new_node(i));
+      assert(contains(head, i));
+   }
+   printf("\t-> basic tests passed\n");
+
+   delete_list(head);
+
+   printf("All tests done.\n");
+
+}
+
+void delete_list(Node list) {
+   if (list) {
+      delete_list(list->next);
+      free(list);
+   }
+}
+
+int contains(Node list, int data) {
+   if (list) {
+      return (list->data == data) || contains(list->next, data);
+   }
+   return 0;
+}
+
+Node delete_list_contains(Node curr, int data) {
+   if (curr) {
+      if (curr->data == data) {
+         Node temp = curr->next;
+         free(curr);
+         curr = temp;
+      } else {
+         curr->next = delete_list_contains(curr->next, data);
+      }
+   }
+   return curr;
+}
+
+int length(Node list) {
+   if (list) {
+      return 1 + length(list->next);
+   }
+   return 0;
+}
+
+Node new_node(int data) {
+   Node n = malloc(sizeof(struct node));
+   if (!n) {
+      fprintf(stderr, "Out of memory, exiting!\n");
+      exit(1);
+   }
+
+   n->data = data;
+   n->next = NULL;
+   return n;
+}
+
+Node list_append(Node curr, Node new) {
+   if (curr) {
+      curr->next = list_append(curr->next, new);
+   } else {
+      return new;
+   }
+   return curr;
+}
+
+Node create_list(int len, int multiple) {
+   Node head = new_node(0);
+   for (int i = 1; i < len; i++) {
+      head = list_append(head, new_node(i * multiple));
+   }
+   return head;
+}
+
+Node merge_sorted(Node list1, Node list2) {
+    if (list1 == NULL) {
+        return list2;
+    } else if (list2 == NULL) {
+        return list1;
+    } else if (list1->data < list2->data) {
+        list1->next = merge_sorted(list1->next, list2);
+        return list1;
+    } else {
+        list2->next = merge_sorted(list1, list2->next);
+        return list2;
+    }
+}
+
+void print_list(Node list) {
+   if (list) {
+      printf("[%d]->", list->data);
+      print_list(list->next);
+
+   } else {
+      printf("[X]\n");
+   }
+}
---
+++ static/comp1511/wednesday/week13/list.h
@@ -0,0 +1,21 @@
+typedef struct node *Node;
+
+struct node {
+    Node    next;
+    int     data;
+};
+
+Node copy(Node head);
+int identical(Node head1, Node head2);
+int ordered(Node head);
+Node set_intersection(Node set1, Node set2);
+Node set_union(Node set1, Node set2);
+Node create_list(int len, int multiple);
+int  contains(Node list, int data);
+int  length(Node list);
+Node merge_sorted(Node list1, Node list2);
+Node new_node(int data);
+Node list_append(Node list1, Node list2);
+void print_list(Node list);
+void delete_list(Node list);
+Node delete_list_contains(Node list, int data);
